86ca1aba09f2374bfc38ac971298a4806946e746
nishant-0411 <itsnishant444@gmail.com>
2026-02-20 19:09:19 +0530

    Fallback prevention system

--- app/services/rag_engine.py
+++ app/services/rag_engine.py
@@ -14,7 +14,8 @@ def retrieve_context(query:str, user_role:str):
             n_results=8
         )
     else:
-        allowed_depts = [dept for dept, roles in role_folder_mapping.items() if user_role in [r.casefold() for r in roles]]
+        allowed_depts = [dept for dept, roles in role_folder_mapping.items() \
+                        if user_role in [r.casefold() for r in roles]]
 
         if not allowed_depts:
              allowed_depts = []
@@ -39,45 +40,55 @@ def generate_response(query: str, user_role: str):
     context = retrieve_context(query, user_role)
 
     if not context:
-        return "No relevant documents found for your role"
-    
-    prompt = f"""
-    You are an internal company knowledge assistant.
+        prompt = f"""
+        You are an AI assistant.
 
-    Answer ONLY using the provided context.
-    Be concise and professional.
-    Do NOT add external knowledge.
-    If the answer is not clearly found, say:
-    "I don't have enough information in the company knowledge base."
+        The user asked:
+        {query}
 
-    Context:
-    {context}
+        There is no relevant information available in the company knowledge base.
 
-    Question:
-    {query}
+        Inform the user politely that no company data was found.
+        Then provide a helpful answer based on general knowledge.
 
-    Answer:
-    """
+        Answer:
+        """
+    else:
+        prompt = f"""
+        You are an internal company knowledge assistant.
 
-    
+        Answer ONLY using the provided context.
+        Be concise and professional.
+        Do NOT add external knowledge.
+        If the answer is not clearly found, say:
+        "I don't have enough information in the company knowledge base."
+
+        Context:
+        {context}
+
+        Question:
+        {query}
+
+        Answer:
+        """
+
     try:
-
         response = requests.post(
             settings.OLLAMA_URL,
-            json = {
+            json={
                 "model": settings.OLLAMA_MODEL,
                 "prompt": prompt,
                 "stream": False
             },
             timeout=60
         )
+
         return response.json()["response"]
-    
+
     except Exception as e:
         error_msg = str(e)
 
         if "429" in error_msg or "Quota exceeded" in error_msg:
             return "I'm currently experiencing high traffic. Please try again in a minute."
-        
-        return f"Error generating response: {error_msg}"
 
+        return f"Error generating response: {error_msg}"
